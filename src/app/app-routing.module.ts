d2139e84394cf4ce15843edb1908ae5c0717465d
Bharati <76688668+bharati7680@users.noreply.github.com>
2022-07-08 16:35:42 +0000

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "orders", component: OrdersComponent
+  },
+  {
+    path: "dashboard", component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
